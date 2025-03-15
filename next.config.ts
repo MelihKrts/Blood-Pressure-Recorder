282e9f41ef25460d9189f7043c57dee011285d9e
MelihKrts <melih.karatas1281@gmail.com>
2025-03-15 15:34:39 +0300

    PWA supporting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,38 @@
+// import withPWA from "next-pwa";
+// import type { NextConfig } from "next";
+//
+// const nextConfig: NextConfig = {
+//     reactStrictMode: true,
+//     swcMinify: true,
+// };
+//
+// // @ts-ignore
+// export default withPWA({
+//     ...nextConfig,
+//
+//     pwa: {
+//         dest: "public",
+//         register: true,
+//         skipWaiting: true,
+//         disable: process.env.NODE_ENV === "development",
+//     },
+// });
+
+
+import withPWA from "next-pwa";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    reactStrictMode: true,
+    swcMinify: true,
 };
 
-export default nextConfig;
+export default withPWA({
+    ...nextConfig,
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === "development",
+    } as any, // TS hatasını önler
+});
