963af4196f77f624f9c53e4d5d6959694aa5050e
MelihKrts <melih.karatas1281@gmail.com>
2026-02-08 20:23:35 +0300

    fix:remove strict validation

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -40,6 +40,7 @@ export async function POST(req: Request) {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
             maxAge: 60 * 60 * 24,
+            sameSite:"lax",
             path: "/",
         })
 
