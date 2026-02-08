ea41777dc0bded8244b246088894f0300afad932
MelihKrts <melih.karatas1281@gmail.com>
2026-02-08 20:39:35 +0300

    feat: migrate database layer to mongodb

--- lib/validation.ts
+++ lib/validation.ts
@@ -3,6 +3,6 @@ import * as z from "zod"
 export const authSchema = z.object({
     email:z.string().min(1,"En az karakter 1 girilmesi gereklidir.").email().trim(),
     password: z.string().min(6,"6 karakterden az şifre olamaz").max(50,"En fazla")
-})
+}).strict()
 
 export type AuthSchemaType = z.infer<typeof authSchema>
