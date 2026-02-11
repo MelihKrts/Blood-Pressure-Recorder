358e68cc3700323854512c2f1c92be8f16f3cd1d
MelihKrts <melih.karatas1281@gmail.com>
2026-02-11 17:08:22 +0300

    feat(seo): add sitemap, robots and social preview assets

---
+++ sitemap.ts
@@ -0,0 +1,24 @@
+import {MetadataRoute} from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: `${process.env.NEXT_PUBLIC_URL}`,
+            lastModified: new Date(),
+            changeFrequency: undefined,
+            priority: 1
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_URL}/login`,
+            lastModified: new Date(),
+            changeFrequency: undefined,
+            priority: 0.9,
+        },
+        {
+            url: `${process.env.NEXT_PUBLIC_URL}/register`,
+            lastModified: new Date(),
+            changeFrequency: undefined,
+            priority: 0.9
+        },
+    ]
+}
